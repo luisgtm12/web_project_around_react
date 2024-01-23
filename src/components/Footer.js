63890670d2a4ebb26516fdd12ce793cfc6f66a63
luisgtm12 <120407727+luisgtm12@users.noreply.github.com>
2024-01-23 12:48:32 -0500

    Correccion de CRA

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+function Footer (){
+  return(
+    <footer className="footer">
+          <h4 className="footer__copyright">© 2021 Alrededor de los EEUU</h4>
+        </footer>
+  )
+}
+export default Footer;
